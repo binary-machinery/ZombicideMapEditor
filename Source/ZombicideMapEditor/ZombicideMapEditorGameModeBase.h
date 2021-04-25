1d9a86c9baa59da105939d8e922d8cb0677063cf
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2021-04-25 16:20:35 +0300

    Initial

---
+++ Source/ZombicideMapEditor/ZombicideMapEditorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombicideMapEditorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBICIDEMAPEDITOR_API AZombicideMapEditorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
