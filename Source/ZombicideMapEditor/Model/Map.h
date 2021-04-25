4935b14d92152434201fa94eeabfbbbd7407a87f
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2021-04-25 16:22:55 +0300

    Model WIP

---
+++ Source/ZombicideMapEditor/Model/Map.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "Tile.h"
+
+namespace Model
+{
+    class FMap : FNoncopyable
+    {
+    public:
+        FMap(const uint32_t SizeX, const uint32_t SizeY)
+            : SizeX(SizeX),
+              SizeY(SizeY)
+        {
+            TileGrid.Reserve(SizeX);
+            for (TArray<const FTile*>& Column : TileGrid)
+            {
+                Column.Reserve(SizeY);
+            }
+        }
+
+        uint32_t GetSizeX() const
+        {
+            return SizeX;
+        }
+
+        uint32_t GetSizeY() const
+        {
+            return SizeY;
+        }
+
+    private:
+        uint32_t SizeX;
+        uint32_t SizeY;
+        TArray<TArray<const FTile*>> TileGrid;
+    };
+}
