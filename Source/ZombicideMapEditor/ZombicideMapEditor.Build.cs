1a33c1670778e71e1e8396d6fcb72cf3d546d1ff
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2021-05-05 22:08:21 +0300

    UI WIP

--- Source/ZombicideMapEditor/ZombicideMapEditor.Build.cs
+++ Source/ZombicideMapEditor/ZombicideMapEditor.Build.cs
@@ -8,7 +8,7 @@ public class ZombicideMapEditor : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
